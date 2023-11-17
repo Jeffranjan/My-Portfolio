6760fac6c3d8961c7f6a444eb14ef30b262f14a4
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-11-17 19:00:19 +0530

    Footer Section Added

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,18 +1,24 @@
-// import { EmailTemplate } from "../../../components/EmailTemplate";
+// import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.From_Email;
 
-export async function GET() {
+export async function POST() {
+  const { body } = req;
+  const { email, subject, message } = body;
   try {
     const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>>",
-      to: ["Ranjanguptajeff@gmail.com"],
+      from: fromEmail,
+      to: ["Ranjanguptajeff@gmail.com", email],
       subject: "Hello world",
       react: (
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted</p>
+          <p>{message}</p>
         </>
       ),
     });
