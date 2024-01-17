01bb6b5f64c8ff4b10285b3cce7489040bddd02b
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2024-01-17 16:46:23 +0530

    Updated Title and Footer Logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='footer border border-t-[#33353f] border-l-transparent border-r-transparent text-white'>
-        <div className='container p-12 flex justify-between'>
-            <span>LOGO</span>
-            <p className='text-slate-600'>All rights reserved</p>
-        </div>
+    <footer className="footer border border-t-[#33353f] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>
+          <img
+            className="h-12 w-12 rounded-full"
+            src="/images/rg-logo.png"
+            alt="logo"
+          />
+        </span>
+        <p className="text-slate-600">All rights reserved</p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
